1140fc33697b5b4ccb68dc33e4986d11df0ce5b3
Andreas G <13819164+agaertner@users.noreply.github.com>
2022-04-19 11:48:35 +0200

    add proj

---
+++ Musician Module/UI/Models/MusicSheet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Nekres.Musician.Core.Domain;
+
+namespace Nekres.Musician_Module.Domain
+{
+    public class MusicSheet
+    {
+        public MusicSheet(string title, string instrument, Metronome metronomeMark, IEnumerable<ChordOffset> melody)
+        {
+            MetronomeMark = metronomeMark;
+            Melody = melody;
+            Instrument = instrument;
+            Title = title;
+        }
+
+        public string Artist { get; }
+
+        public string Title { get; }
+
+        public string User { get; }
+
+        public string Instrument { get; }
+
+        public Metronome MetronomeMark { get; }
+
+        public IEnumerable<ChordOffset> Melody { get; }
+    }
+}
